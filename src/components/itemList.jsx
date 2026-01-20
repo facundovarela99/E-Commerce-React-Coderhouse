b69fd0a2713d6d361d875454ae9482c0c4a8a150
Facundo Varela <facundovarela88@hotmail.com>
2026-01-20 20:44:12 -0300

    Instalando tailwind

--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -4,6 +4,9 @@ import { Item } from './item.jsx'
 export function ItemList({items}){
     return(<div className={styles.divItemListContainer}>
             <div className={styles.divGames}>
+                <h1 class="text-3xl font-bold underline m-5">
+                    Hello world!
+                </h1>
                 {items.map(videojuego => (
                     <Item videojuego={videojuego} key={videojuego.id}></Item>
                 ))}
